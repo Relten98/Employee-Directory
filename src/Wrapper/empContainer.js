b99b650fe81c72ab952c56a39bdda21bdc1d528f
Relten98 <diezhandinoty@gmail.com>
2021-03-26 17:09:53 -0700

    Basic updates
    Removing the example template files from the repo, since they aren't going to be used anyways.

---
+++ src/Wrapper/empContainer.js
@@ -0,0 +1,19 @@
+// Imports for react.
+import React from 'react';
+import '../Wrapper/style.css';
+import API from '../utils/api.js';
+
+// The core empContainer that will be used to fill out with employees.
+class empContainer extends React.Component {
+    state = {
+        users: [],
+        search: '',
+        // This is going to be important for sorting later on.
+        sortby: '',
+        col: '',
+    };
+
+
+}
+
+// OOOOOWA-AH-AH-AH
--- src/Wrapper/employee.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Employee(props) {
-
-    return <div className="employee">{props.employee}</div>;
-
-}
